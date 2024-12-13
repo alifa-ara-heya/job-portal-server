70fa3099167bc8ee8852563115ffa34290379e1a
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-12-12 19:12:22 -0500

    job delete button functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -34,6 +34,7 @@ async function run() {
 
         // jobs related apis
         const jobsCollection = client.db('jobPortal').collection('jobs')
+        const jobApplicationCollection = client.db('jobPortal').collection('job-applications')
 
         // get the data
         app.get('/jobs', async (req, res) => {
@@ -42,6 +43,61 @@ async function run() {
             res.send(result);
         })
 
+        // get the individual data
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query);
+            res.send(result)
+        })
+
+
+        // job application apis
+        // get one/all/many data
+        // app.get('/job-applications', async (req, res) => {
+        //     const cursor = jobApplicationCollection.find();
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+
+        // getting job applications with the email
+        app.get('/job-applications', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant_email: email }
+            const result = await jobApplicationCollection.find(query).toArray();
+
+            // fokira way to aggregate data 
+            // because you will not have all the job information here, just the job id;
+            for (const application of result) {
+                // console.log(application.job_id);
+                const query = { _id: new ObjectId(application.job_id) }
+                const result = await jobsCollection.findOne(query);
+                if (result) {
+                    application.title = result.title;
+                    application.company = result.company;
+                    application.company_logo = result.company_logo;
+                    application.location = result.location
+                }
+            }
+
+            res.send(result);
+        })
+
+        // delete my job
+        app.delete('/job-applications/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobApplicationCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // post my job
+
+        app.post('/job-applications', async (req, res) => {
+            const application = req.body;
+            const result = await jobApplicationCollection.insertOne(application);
+            res.send(result);
+        })
 
     } finally {
         // Ensures that the client will close when you finish/error
